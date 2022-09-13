82c0129949e56209567f29773ef2323eb0463e6f
Grrom <josejeromelalunio@gmail.com>
2022-09-13 09:44:23 +0800

    add login

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import multer from "multer";
 import SauceNaoHelper from "./saucenao";
+import cors from "cors";
 import "dotenv/config";
+import SuccessResponse from "./types/success-response";
 
 const app = express();
 const upload = multer();
 const sauceNaoHelper = new SauceNaoHelper(process.env.SAUCENAO!);
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 app.use(express.json());
 
 app.post("/sauce", upload.single("image"), async (req, res) => {
@@ -20,6 +24,17 @@ app.post("/sauce", upload.single("image"), async (req, res) => {
     response = { data: [], error: "Image is required!" };
   }
   res.send(response);
+});
+
+app.post("/login", async (req, res) => {
+  setTimeout(() => {
+    // res.send(new SuccessResponse(false, "Failed to login"));
+    res.send(new SuccessResponse(true, "Logged in successfully."));
+  }, 1000);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(process.env.PORT);
