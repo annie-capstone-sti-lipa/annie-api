c2f96838ce361cf4b7ad24bbe02c33b6073d8f2e
Grrom <josejeromelalunio@gmail.com>
2022-09-27 14:04:18 +0800

    add build

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new Dotenv()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.join(__dirname, "bundle"),
+    filename: "bundle.js",
+  },
+  optimization: {
+    minimize: true,
+  },
+};
